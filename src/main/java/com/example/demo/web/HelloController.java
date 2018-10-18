aeb1f610edabc20050bc839a952a09751b85ea45
daizhe <Administrator@SKY-20180125WPI>
2018-10-18 09:52:08 +0800

    123

--- src/main/java/com/example/demo/web/HelloController.java
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -3,10 +3,15 @@ package com.example.demo.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
 
 
 @RestController
+@Slf4j
 public class HelloController {
 
 	@Autowired
@@ -15,8 +20,20 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String index(){
 		  String services = "Services: " + client.getServices();
+		  log.info("meta："+client.getLocalServiceInstance().getMetadata());
 	        System.out.println(services);
 	        return services;
 		
 	}
+	
+	 @Autowired
+	 MyHealthIndicator myHealthChecker;
+	 
+
+	    @RequestMapping("/up")
+	    public String up(@RequestParam("up") Boolean up) {
+	        myHealthChecker.setUp(up);
+	 
+	        return up.toString();
+	    }
 }
